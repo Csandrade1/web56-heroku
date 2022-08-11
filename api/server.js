b90272915b6b577eb89f56e23666458f962fc434
Csandrade1 <andrade.cynthia07@gmail.com>
2022-08-11 16:13:19 -0700

    prepare for heroku

--- api/server.js
+++ api/server.js
@@ -7,4 +7,8 @@ server.use(express.json());
 server.get("/", (req, res) => {
   res.send("Hello, world!");
 });
+
+server.post("/", (req, res) => {
+  res.json(req.body);
+});
 module.exports = server;
